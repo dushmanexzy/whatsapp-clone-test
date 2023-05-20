2529e7ec0fcd2d91ae8e0082a755dd2c1c1cad02
Andrey Dvortsov <exzy92@gmail.com>
2023-05-20 17:08:30 +0300

    Splash and ZuzApp components are added

---
+++ src/pages/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../images/logo.svg'
+
+function Splash(props) {
+  return (
+    <div className={'splash abs abc'}>
+      <img className={`abs abc`} src={Logo} alt="Logo"/>
+    </div>
+  );
+}
+
+export default Splash;
