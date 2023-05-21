0116dd2cddee03347575934d723ee8a7460269a5
Andrey Dvortsov <exzy92@gmail.com>
2023-05-21 16:45:35 +0300

    comment for code added

--- src/redux/reducers/appReducer.js
+++ src/redux/reducers/appReducer.js
@@ -1,6 +1,7 @@
 const initialState = {
   debug: true,
   loaded: true,
+  // авторизация до конца не настроена, поэтому изменив значение на True можно посмотреть внешний вид мессенджера
   session: false
 }
 
